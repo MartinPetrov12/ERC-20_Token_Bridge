8391ab9e503a07a72168862ea2c4e0056e862e55
Martin Petrov <m.a.petrov@student.tudelft.nl>
2023-06-21 07:02:59 +0200

    Add lock and token functionalities

---
+++ bridge/scripts/utils.ts
@@ -0,0 +1,26 @@
+import { Wallet, providers } from "ethers";
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.dev' });
+
+export const getProvider = async (bridgeNetwork: string) => {
+
+    if(bridgeNetwork.toLowerCase() == "sepolia") {
+        return new providers.InfuraProvider("sepolia", process.env.INFURA_SEPOLIA_URL);
+    } else if(bridgeNetwork.toLowerCase() == "mumbai") {
+        const provider = new providers.AlchemyProvider('maticmum', process.env.ALCHEMY_MUMBAI_URL);
+        return provider;
+    } else {
+       throw Error("Network " + bridgeNetwork + " is not supported.");
+    }
+}
+
+export const getWallet = async (bridgeNetwork: string, provider: providers.UrlJsonRpcProvider) => {
+    if(bridgeNetwork.toLowerCase() == "sepolia") {
+        return new Wallet(process.env.SEPOLIA_PRIVATE_KEY || '', provider);
+    } else if(bridgeNetwork.toLowerCase() == "mumbai") {
+        return new Wallet(process.env.MUMBAI_PRIVATE_KEY || '', provider);
+    } else {
+       throw Error("Network " + bridgeNetwork + " is not supported.");
+    }
+}
