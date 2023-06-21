d99ccd9edcb9d0c3f4e2de79f369f5b1f12a2613
Martin Petrov <m.a.petrov@student.tudelft.nl>
2023-06-21 11:20:57 +0200

    Add working lock | Add utils | Add token interactions

--- bridge/scripts/deploy-token.ts
+++ bridge/scripts/deploy-token.ts
@@ -17,13 +17,17 @@ export const tokenDeploy = async (tokenName: string, tokenSymbol: string) => {
 }
 
 export const tokenMint = async (bridgeNetwork: string, receiverAddress: string, tokenAddress: string, amount: number) => {
+    console.log(bridgeNetwork);
+    console.log(receiverAddress);
+    console.log(tokenAddress);
+    console.log(amount);
     const provider = await getProvider(bridgeNetwork);
      
     const account = await getWallet(bridgeNetwork, provider);
      
     const tokenContract = new Contract(tokenAddress, GenericTokenArtifact.abi, provider);
      
-    const testTx = await tokenContract.populateTransaction.approve(receiverAddress, amount);
+    const testTx = await tokenContract.populateTransaction.mint(receiverAddress, amount);
     
     testTx.nonce = await account.getTransactionCount();
     if(bridgeNetwork == "mumbai") {
@@ -71,4 +75,30 @@ export const tokenApprove = async (bridgeNetwork: string, spender: string, token
     }    
 }
 
-export default {tokenDeploy, tokenMint};
+export const checkTokenBalance = async (bridgeNetwork: string, userAddress: string, tokenAddress: string ) => {
+    const provider = await getProvider(bridgeNetwork);
+    // const account = await getWallet(bridgeNetwork, provider);
+     
+    const tokenContract = new Contract(tokenAddress, GenericTokenArtifact.abi, provider);
+     
+    const balance = await tokenContract.balanceOf(userAddress);
+    console.log("Balance is " + balance);
+    // if(bridgeNetwork == "mumbai") {
+    //     testTx.chainId = 80001
+    // }
+
+    // const approveTxSigned = await account.signTransaction(testTx);
+    
+    // const submittedTx = await provider.sendTransaction(approveTxSigned);
+    
+    // const approveReceipt = await submittedTx.wait();
+    // if (approveReceipt.status != 1) {
+    //     throw Error("Tokens were not approved");
+    // } else {
+    //     console.log();
+    // } 
+}
+
+
+
+export default {tokenDeploy, tokenMint, tokenApprove, checkTokenBalance};
